965c2dd23d2189623a575eac187ac254b8600b60
23405 <234055231@qq.com>
2023-04-07 11:03:59 +0800

    Changes

---
+++ 01.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+import time
+from selenium import webdriver
+
+
+class TestWebdriver:
+
+    def setup(self):
+        self.driver = webdriver.Edge()
+        self.driver.get('https://www.baidu.com/')
+
+    def teardown(self):
+        self.driver.quit()
+
+    def test_web(self):
+        self.driver.find_element(By.ID, 'kw').send_keys('baidu')
+        self.driver.find_element(By.ID, 'su').click()
+        time.sleep(3)
