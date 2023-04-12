1f2e94a08f5fc86f29290f4e4338d64895010995
23405 <234055231@qq.com>
2023-04-12 17:22:48 +0800

    Changes

--- test_web.py
+++ test_web.py
@@ -14,6 +14,12 @@ class TestWebdriver:
         self.driver.quit()
 
     def test_web(self):
-        self.driver.find_element(By.ID, 'kw').send_keys('baidu')
-        self.driver.find_element(By.ID, 'su').click()
+        self.driver.find_element(By.CSS_SELECTOR, '#kw').send_keys('霍格沃兹测试学院')
+        self.driver.find_element(By.CSS_SELECTOR, '#su').click()
+        time.sleep(1)
+        # 滚动到页面底部
+        self.driver.execute_script('document.documentElement.scrollTop=10000')
+        time.sleep(3)
+        # 点击下一页
+        self.driver.find_element(By.CSS_SELECTOR, '#page > div > a.n').click()
         time.sleep(3)
